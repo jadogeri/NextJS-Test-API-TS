454028fc4b996dbc6a79a0d7c8a13ab5eecfffaf
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-15 12:34:52 -0500

    Implemented 'delete all users' endpoint

--- server/app/api/users/route.ts
+++ server/app/api/users/route.ts
@@ -3,10 +3,11 @@ import { dirname } from 'path';
 import users from "@/app/database/users.json"
 import { NextResponse } from "next/server";
 import fs from "fs"
+import { User } from '@/types/User';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const dataPath = __dirname + "\\..\\..\\util\\users.json"
+const dataPath = __dirname + "\\..\\..\\database\\users.json"
 
 
 export function GET(_: any, response : any){
@@ -17,4 +18,16 @@ export function GET(_: any, response : any){
     return [];
   }
   
+}
+
+export function DELETE(request : any, response : any){
+  try {
+    console.log(users)
+    const noUsers : User[] = [];
+    fs.writeFileSync(dataPath, JSON.stringify(noUsers), 'utf8');
+    return NextResponse.json(noUsers)
+  } catch (err) {
+    console.error('Error reading users:', err);
+    return [];
+  }
 }
