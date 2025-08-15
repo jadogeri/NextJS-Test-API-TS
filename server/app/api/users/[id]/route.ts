ab7a982b882e44af6c8a01a64db77cef54cafd1c
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-15 13:43:35 -0500

    Implemented PATCH request 'modify single user'

--- server/app/api/users/[id]/route.ts
+++ server/app/api/users/[id]/route.ts
@@ -59,3 +59,34 @@ export async function PUT(request: any, response: any){
        return NextResponse.json({ message : `User not found for replace with ID: ${id}`}, {status: 404})
     }
 }
+
+export async function PATCH(request: any, response: any){
+    const { id } = await response.params;
+    const foundUser : User[] = usersArray.filter(user => user.id == id);
+    const {username, age, email, isActive } = await request.json();
+
+    let data : any = {username, age, email, isActive }
+    for (const key in data) {
+        if (data[key] === undefined) {
+            delete data[key];
+        }
+    }
+
+    console.log("data to update", data)
+    if(foundUser.length > 0){
+        const updatedUser : User = {...foundUser[0], ...data}
+        const newUsersList: User[] = usersArray.map((user)=>{
+            if(user.id == updatedUser.id){
+                return updatedUser;
+            }else{
+                return user;
+            }              
+        })
+        fs.writeFileSync(dataPath, JSON.stringify(newUsersList, null, 2), 'utf8');
+        return NextResponse.json(updatedUser, {status: 200})
+
+    }else{
+       return NextResponse.json({ message : `User not found for update with ID: ${id}`}, {status: 404})
+    }
+}
+
